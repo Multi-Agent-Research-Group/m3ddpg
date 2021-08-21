94c7e26ca85d3da01542da45702a99ae25f7e84e
Sriyash <poddarsriyash@gmail.com>
2021-08-21 21:31:53 +0530

    run script improved

--- run.sh
+++ run.sh
@@ -7,63 +7,82 @@ do
         do
             for bp in 'maddpg' 'mmmmddpg'
             do
-                for sm in 0 0.5 1.0
-                do
-                    echo "results/$env/$gp-$bp-$sm/$seed/"
+                echo "results/$env/$gp-$bp-'0'/$seed/"
 
                     python experiments/train.py \
                     --scenario $env \
-                    --max-episode-len 50 \
-                    --num_units 256 \
                     --seed $seed \
                     --num-adversaries 1 \
                     --good-policy $gp \
                     --bad-policy $bp \
-                    --average_perf_wt $sm \
+                    --average_perf_wt 0 \
                     --lr 0.001 \
                     --adv-eps 0.001 \
                     --adv-eps-s 0.001 \
-                    --plots-dir results/$env/$gp-$bp-$sm/$seed/ \
+                    --plots-dir results/$env/$gp-$bp-'0'/$seed/ \
                     --exp-name run &
 
-                    echo " "
-                done
-                    echo "results/$env/$gp-$bp-$sm/$seed/"
+                echo " "
+
+                for sp in 10 100
+                do
+                    for sm in 0.5 1.0
+                    do
+                        echo "results/$env/$gp-$bp-$sm-$sp/$seed/"
+
+                        python experiments/train.py \
+                        --scenario $env \
+                        --seed $seed \
+                        --num-adversaries 1 \
+                        --good-policy $gp \
+                        --bad-policy $bp \
+                        --average_perf_wt $sm \
+                        --lr 0.001 \
+                        --num_samples $sp \
+                        --adv-eps 0.001 \
+                        --adv-eps-s 0.001 \
+                        --plots-dir results/$env/$gp-$bp-$sm-$sp/$seed/ \
+                        --exp-name run &
+
+                        echo " "
+                    done
+
+                    echo "results/$env/$gp-$bp-$sp-'k-min'/$seed/"
 
                     python experiments/train.py \
                     --scenario $env \
-                    --max-episode-len 50 \
-                    --num_units 256 \
                     --seed $seed \
                     --num-adversaries 1 \
                     --good-policy $gp \
                     --bad-policy $bp \
                     --average_perf_wt 1.0 \
                     --lr 0.001 \
+                    --num_samples $sp \
                     --adv-eps 0.001 \
                     --adv-eps-s 0.001 \
-                    --plots-dir results/$env/$gp-$bp-'k-min'/$seed/ \
+                    --plots-dir results/$env/$gp-$bp-$sp-'k-min'/$seed/ \
                     --k_minima \
                     --exp-name run &
 
                     echo " "
+
+                done
             done
         done
-            echo results/$env/'maddpg'-'maddpg'/$seed/
+        
+        echo results/$env/'maddpg'-'maddpg'/$seed/
 
-            python experiments/train.py \
-            --scenario $env \
-            --max-episode-len 50 \
-            --num_units 256 \
-            --seed $seed \
-            --num-adversaries 1 \
-            --lr 0.001 \
-            --adv-eps 0.001 \
-            --adv-eps-s 0.001 \
-            --plots-dir results/$env/'maddpg'-'maddpg'/$seed/ \
-            --exp-name run &
+        python experiments/train.py \
+        --scenario $env \
+        --seed $seed \
+        --num-adversaries 1 \
+        --lr 0.001 \
+        --adv-eps 0.001 \
+        --adv-eps-s 0.001 \
+        --plots-dir results/$env/'maddpg'-'maddpg'/$seed/ \
+        --exp-name run &
 
-            echo " "
+        echo " "
     done
     wait
 done
